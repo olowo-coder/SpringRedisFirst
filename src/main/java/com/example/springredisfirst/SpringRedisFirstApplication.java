1b331b2224c7f56f79475e8e67eeed5d8ceb5406
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-01-30 00:26:50 +0100

    dfs

---
+++ src/main/java/com/example/springredisfirst/SpringRedisFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.springredisfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisFirstApplication.class, args);
+    }
+
+}
